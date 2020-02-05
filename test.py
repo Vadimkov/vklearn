4f920657aec665581e2f1545926da55918587ff8
vkovalev <vd.kv@ya.ru>
2020-02-05 21:37:01 +0300

    KNN and LinearRegression first versionas

--- test.py
+++ test.py
@@ -23,8 +23,81 @@ def test_KNeighborsClassifier():
     Y = klf.predict(X_test)
 
     from sklearn.metrics import accuracy_score
-    print("accuracy_score: %d" % (accuracy_score(Y, Y_test)))
+    print("test_KNeighborsClassifier. accuracy_score: %d" % (accuracy_score(Y, Y_test)))
+
+def test_LinearRegressionCoordDescent():
+    from sklearn import datasets
+    diabetes_X, diabetes_y = datasets.load_diabetes(return_X_y=True)
+
+    # Split the data into training/testing sets
+    diabetes_X_train = diabetes_X[:-20]
+    diabetes_X_test = diabetes_X[-20:]
+
+    # Split the targets into training/testing sets
+    diabetes_y_train = diabetes_y[:-20]
+    diabetes_y_test = diabetes_y[-20:]
+
+    clnr = vk.LinearRegression(10000)
+    clnr.fit(diabetes_X_train, diabetes_y_train, "coord_descent")
+
+    Y = clnr.predict(diabetes_X_test)
+    print("Y type:", type(Y), " Len:", len(Y))
+    print("diabetes_y_test type:", type(diabetes_y_test), " Len:", len(diabetes_y_test))
+
+    print("Y[0]", Y[0])
+    print("diabetes_y_test[0]", diabetes_y_test[0])
+
+    from sklearn.metrics import mean_squared_error
+    error = mean_squared_error(Y, diabetes_y_test)
+    print("test_LinearRegressionCoordDescent. mean_squared_error: %d" % (error))
+
+def test_LinearRegressionGradDescent():
+    from sklearn import datasets
+    diabetes_X, diabetes_y = datasets.load_diabetes(return_X_y=True)
+
+    # Split the data into training/testing sets
+    diabetes_X_train = diabetes_X[:-20]
+    diabetes_X_test = diabetes_X[-20:]
+
+    # Split the targets into training/testing sets
+    diabetes_y_train = diabetes_y[:-20]
+    diabetes_y_test = diabetes_y[-20:]
+
+    glnr = vk.LinearRegression(10000)
+    glnr.fit(diabetes_X_train, diabetes_y_train, "grad_descent")
+
+    Y = glnr.predict(diabetes_X_test)
+
+    from sklearn.metrics import mean_squared_error
+    error = mean_squared_error(Y, diabetes_y_test)
+    print("test_LinearRegressionGradDescent. mean_squared_error: %d" % (error))
+
+
+def test_LinearRegressionFromSKLearn():
+    from sklearn import datasets
+    diabetes_X, diabetes_y = datasets.load_diabetes(return_X_y=True)
+
+    # Split the data into training/testing sets
+    diabetes_X_train = diabetes_X[:-20]
+    diabetes_X_test = diabetes_X[-20:]
+
+    # Split the targets into training/testing sets
+    diabetes_y_train = diabetes_y[:-20]
+    diabetes_y_test = diabetes_y[-20:]
+
+    from sklearn.linear_model import LinearRegression
+    sklnr = LinearRegression()
+    sklnr.fit(diabetes_X_train, diabetes_y_train)
+
+    Y = sklnr.predict(diabetes_X_test)
+
+    from sklearn.metrics import mean_squared_error
+    error = mean_squared_error(Y, diabetes_y_test)
+    print("test_LinearRegressionFromSKLearn. mean_squared_error: %d" % (error))
 
 
 if __name__ == "__main__":
     test_KNeighborsClassifier()
+    test_LinearRegressionCoordDescent()
+    test_LinearRegressionGradDescent()
+    test_LinearRegressionFromSKLearn()
--- vklearn.py
+++ vklearn.py
@@ -1,23 +1,52 @@
 import numpy as np
 
+
 package_name = "vklearn"
+
+
+DELTA = 0.0000000001
+
+
+def euclidian_distance(v1, v2):
+    return abs(sum((v1 - v2)**2)**0.5)
+
+
+def get_standard_error(features, targets, weights, b):
+    standard_error_sum = 0.
+    for feature, target in zip(features, targets):
+        standard_error_sum += ((np.sum(weights * feature) + b) - target) ** 2
+    standard_error = standard_error_sum / len(features)
+    return standard_error
+
+
+def grad_gse(f, features, targets, weights, b):
+    f_val = f(features, targets, weights, b)
+    
+    gradient = np.zeros((len(weights) + 1))
+    w = list(weights)
+    w.append(b)
+    point = np.array(w)
+
+    for i in range(len(point)):
+        delta_point = list(point)
+        delta_point[i] += DELTA
+        delta_val = f(features, targets, delta_point[:-1], delta_point[-1])
+        gradient[i] = (delta_val - f_val) / DELTA
+    
+    return gradient
+
 
 class KNeighborsClassifier:
 
     def __init__(self, n_neighbors = 5):
-        print("%s.KNeighborsClassifier created" % (package_name))
-
         self.n_neighbors = n_neighbors
     
     def fit(self, features, targets):
-        print("fit  %d objects" % (len(features)))
-
         if len(features) != len(targets):
             raise AttributeError("Features number '%d' should be equal to targets number '%d'" % (len(features), len(targets)))
         
         self.features = features
         self.targets = targets
-        print("Targets:", self.targets)
 
     def predict(self, X):
         Y = list()
@@ -31,7 +60,6 @@ class KNeighborsClassifier:
 
             distances = distances[:self.n_neighbors]
             results = dict()
-            print(distances)
             for d in distances:
                 neighbor_class = d[1]
                 if neighbor_class not in results:
@@ -46,5 +74,71 @@ class KNeighborsClassifier:
         return Y
 
 
-def euclidian_distance(v1, v2):
-    return abs(sum((v1 - v2)**2)**0.5)
+class LinearRegression:
+
+    def __init__(self, max_number_of_iterations = 10000):
+        self._max_number_of_iterations = max_number_of_iterations
+
+    def fit(self, features, targets, fit_model = "grad_descent"):
+        if fit_model == "coord_descent":
+            self._fit_by_coord_descent(features, targets)
+        elif fit_model == "grad_descent":
+            self._fit_by_grad_descent(features, targets)
+    
+    def predict(self, X):
+        Y = np.zeros(len(X))
+        for i in range(len(X)):
+            Y[i] = np.sum(self._weights * X[i]) + self._b
+        
+        return Y
+
+    
+    def _fit_by_coord_descent(self, features, targets):
+        self._weights = np.full(len(features[0]), 5)
+        self._b = 200
+
+        deltas = np.full(len(self._weights), 5)
+        previous_error = get_standard_error(features, targets, self._weights, self._b)
+        iteration_error = previous_error
+
+        for i in range(self._max_number_of_iterations):
+            for w in range(len(self._weights)):
+                self._weights[w] += deltas[w]
+                current_error = get_standard_error(features, targets, self._weights, self._b)
+
+                if current_error > previous_error:
+                    deltas[w] *= -0.5
+                previous_error = current_error
+
+            current_error = get_standard_error(features, targets, self._weights, self._b)
+            if current_error > previous_error:
+                self._b *= -0.5
+            previous_error = current_error
+
+            if i % 100 == 0 and i != 0:               
+                if (previous_error - iteration_error) < 0.00001:
+                    break
+                iteration_error = previous_error
+        
+    def _fit_by_grad_descent(self, features, targets):
+        self._weights = np.full(len(features[0]), 5)
+        self._b = 200
+
+        stepsize = 0.2
+        steps = np.full(len(self._weights), stepsize)
+        previous_error = get_standard_error(features, targets, self._weights, self._b)
+        iteration_error = previous_error
+
+        for i in range(self._max_number_of_iterations):
+            current_error = get_standard_error(features, targets, self._weights, self._b)
+            g = grad_gse(get_standard_error, features, targets, self._weights, self._b)
+            g *= (-1.0 * stepsize)
+            self._weights = self._weights + g[:-1]
+            self._b += g[-1]
+                
+            previous_error = current_error
+
+            if i % 100 == 0 and i != 0:
+                if abs(previous_error - iteration_error) < 0.000000001:
+                    break
+                iteration_error = previous_error
